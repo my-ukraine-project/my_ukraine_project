ae4316b742ad615788f37419842973b56812fe67
Ivan Zatevaxin <zatevaxin.ivan@gmail.com>
2017-05-05 09:08:05 +0300

    Метод 'get_user_by_session' перенесен в родительский класс.

--- application/core/model.php
+++ application/core/model.php
@@ -16,4 +16,26 @@ class Model {
     public function escape($string) {
         return $this->mysqli->real_escape_string(trim(htmlspecialchars(trim($string))));
     }
+
+    public function get_user_by_session() {
+        if (empty($_COOKIE['uid'])) {
+            return null;
+        }
+
+        $cookie = $this->escape($_COOKIE['uid']);
+
+        $ret = $this->mysqli->query(
+            "SELECT u.* FROM
+                      Users AS u JOIN User_Session AS s ON u.id = s.user_id 
+                    WHERE s.cookie = '$cookie' LIMIT 1"
+        );
+
+        return ($ret && ($ret->num_rows == 1)) ? $ret->fetch_object() : null;
+    }
+
+    public function check_permission() {
+        $data = $this->get_user_by_session();
+        return $data ? !!$data->permission : false;
+    }
+
 }
--- application/models/model_main.php
+++ application/models/model_main.php
@@ -17,20 +17,5 @@ class Model_Main extends Model {
         );
     }
 
-    public function get_user_by_session() {
-        if (empty($_COOKIE['uid'])) {
-            return null;
-        }
-
-        $cookie = $this->escape($_COOKIE['uid']);
-
-        $ret = $this->mysqli->query(
-            "SELECT u.* FROM
-                      Users AS u JOIN User_Session AS s ON u.id = s.user_id 
-                    WHERE s.cookie = '$cookie' LIMIT 1"
-        );
-
-        return ($ret && ($ret->num_rows == 1)) ? $ret->fetch_object() : null;
-    }
 
 }
