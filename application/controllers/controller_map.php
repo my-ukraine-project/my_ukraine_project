417230f7dea42f41124896c7421dbecd33c4c437
Ivan Zatevaxin <zatevaxin.ivan@gmail.com>
2017-05-14 00:37:13 +0300

    Доделан обработчик страницы с картой.

--- application/controllers/controller_map.php
+++ application/controllers/controller_map.php
@@ -2,7 +2,18 @@
 
 class Controller_Map extends Controller {
 
+    function __construct() {
+        parent::__construct();
+        $this->model = new Model();
+    }
+
     function action_index() {
-        $this->view->generate('map_view.php', 'template_view.php');
+        $data = $this->model->get_user_by_session();
+        if (!$data) {
+            Route::redirect("/");
+            return;
+        }
+
+        $this->view->generate('map_view.php', 'template_view.php', $data);
     }
 }
